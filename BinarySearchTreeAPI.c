4406b7ef0446b15f9345d9ee4e47b6e256b35e03
Shawn <45508693+xWink@users.noreply.github.com>
2020-03-20 06:25:59 -0400

    Add files via upload

---
+++ BinarySearchTreeAPI.c
@@ -0,0 +1,198 @@
+/**
+ ************************
+ * Name: Shawn Kaplan
+ * Date: March. 19, 2020
+ ************************
+**/
+
+#include "BinarySearchTreeAPI.h"
+
+BST* createBST(int dataSize, int (*compareFunction)(const void* a, const void* b), void (*deleteFunction)(void* data), void (*printFunction)(void* data)) {
+    if (!compareFunction || !deleteFunction || dataSize < 0) {
+        return NULL;
+    }
+    
+    BST* tree = malloc(sizeof(BST));
+    tree->root = NULL;
+    tree->dataSize = dataSize;
+    tree->compareFunction = compareFunction;
+    tree->deleteFunction = deleteFunction;
+    tree->printFunction = printFunction;
+    
+    return tree;
+}
+
+void deleteBST(BST* tree) {
+    if (!tree) {
+        return;
+    }
+
+    deleteSubtree(tree->root, tree->deleteFunction);
+    free(tree);
+}
+
+void printBST(BST* tree) {
+    printSubtree(tree->root, tree->printFunction);
+}
+
+void printSubtree(BSTNode* node, void (*printFunction)(void* data)) {
+    if (!node) {
+        return;
+    }
+    
+    if (node->left) {
+        printSubtree(node->left, printFunction);
+    }
+    
+    if (node->data) {
+        printFunction(node->data);
+    }
+    
+    if (node->right) {
+        printSubtree(node->right, printFunction);
+    }
+}
+
+BSTNode* createBSTNode(void* data) {
+    BSTNode* node = malloc(sizeof(BSTNode));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+void deleteSubtree(BSTNode* node, void (*deleteFunction)(void* data)) {
+    if (!node || !deleteFunction) {
+        return;
+    }
+
+    // Delete child nodes
+    deleteSubtree(node->left, deleteFunction);
+    deleteSubtree(node->right, deleteFunction);
+    
+    // Delete this node
+    if (node->data) {
+        deleteFunction(node->data);
+    }
+    free(node);
+}
+
+void insertData(BST* tree, void* data) {
+    if (!tree || !data) {
+        return;
+    }
+    
+    BSTNode* currNode = tree->root;
+    BSTNode* parentNode = tree->root;
+    BSTNode* newNode = createBSTNode(data);
+    
+    // If there is no root, new node becomes the root
+    if (!tree->root) {
+        tree->root = newNode;
+        return;
+    }
+    
+    while (1) {
+        parentNode = currNode;
+        // Check left side
+        if (tree->compareFunction(data, parentNode->data) < 0) {
+            if ((currNode = currNode->left) == NULL) {
+                parentNode->left = newNode;
+                return;
+            }
+        }
+        // Check right side
+        else {
+            if ((currNode = currNode->right) == NULL) {
+                parentNode->right = newNode;
+                return;
+            }
+        }
+    }
+}
+
+void deleteData(BST* tree, void* data) {
+    tree->root = deleteDataRecursive(tree, tree->root, data);
+}
+
+BSTNode* deleteDataRecursive(BST* tree, BSTNode* node, void* data) {
+    if (!tree || !node) {
+        return node;
+    }
+    
+    // Check left children
+    if (tree->compareFunction(data, node->data) < 0) {
+        node->left = deleteDataRecursive(tree, node->left, data);
+        return node;
+    }
+    // Check right children
+    else if (tree->compareFunction(data, node->data) > 0) {
+        node->right = deleteDataRecursive(tree, node->right, data);
+        return node;
+    }
+    // Found match
+    else {
+        // If < 2 children
+        if (!node->left) {
+            BSTNode* temp = node->right;
+            tree->deleteFunction(node->data);
+            free(node);
+            node = NULL;
+            return temp;
+        } else if (!node->right) {
+            BSTNode* temp = node->left;
+            tree->deleteFunction(node->data);
+            free(node);
+            node = NULL;
+            return temp;
+        }
+        
+        // If 2 children
+        BSTNode* temp = findMin(node->right);
+        memcpy(node->data, temp->data, tree->dataSize);
+        node->right = deleteDataRecursive(tree, node->right, temp->data);
+    } 
+    return node;
+}
+
+BSTNode* findMin(BSTNode* node) {
+    if (node && node->left) {
+        return findMin(node->left);
+    }
+    
+    return node;
+}
+
+BSTNode* findData(BST* tree, void* data) {
+    if (!tree) {
+        return NULL;
+    }
+    
+    BSTNode* currNode = tree->root;
+    BSTNode* parentNode = tree->root;
+    
+    // If there is no root, return NULL
+    if (!tree->root) {
+        return NULL;
+    }
+
+    while (1) {
+        parentNode = currNode;
+        // Check left side
+        if (tree->compareFunction(data, parentNode->data) < 0) {
+            if ((currNode = currNode->left) == NULL) {
+                return NULL;
+            }
+        }
+        // Check right side
+        else if (tree->compareFunction(data, parentNode->data) > 0) {
+            if ((currNode = currNode->right) == NULL) {
+                return NULL;
+            }
+        }
+        // Return match
+        else {
+            return currNode;
+        }
+    }
+}
---
+++ BinarySearchTreeAPI.h
@@ -0,0 +1,119 @@
+/**
+ ************************
+ * Name: Shawn Kaplan
+ * ID: 9066499
+ * Date: March. 19, 2020
+ * Assignment: A4
+ ************************
+**/
+
+#ifndef BST_API_H
+#define BST_API_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct bstNode {
+    void* data;
+    struct bstNode* left;
+    struct bstNode* right;
+} BSTNode;
+
+typedef struct bst {
+    BSTNode* root;
+    int dataSize;
+    int (*compareFunction)(const void* a, const void* b);
+    void (*deleteFunction)(void* data);
+    void (*printFunction)(void* data);
+} BST;
+
+/**
+ * Creates a new binary search tree.
+ * @param dataSize - the size of the datatype stored in the tree
+ * @param compareFunction - a pointer to a function used to compare stored data
+ * @param deleteFunction - a pointer to a function used to delete stored data
+ * @param printFunction - a pointer to a function used to print stored data
+ * @return a pointer to a newly allocated binary search tree
+ */
+BST* createBST(
+    int dataSize,
+    int (*compareFunction)(const void* a, const void* b),
+    void (*deleteFunction)(void* data),
+    void (*printFunction)(void* data));
+
+/**
+ * Deletes an entire binary search tree, including its data.
+ * @param tree - the tree to be deleted
+ */
+void deleteBST(BST* tree);
+
+/**
+ * Deletes a subtree of a binary search tree.
+ * @param node - the root of the subtree
+ * @param deleteFunction - a pointer to the function used to delete node data
+ */
+void deleteSubtree(BSTNode* node, void (*deleteFunction)(void* data));
+
+/**
+ * Prints the entire contents of a binary tree in ascending order.
+ * @param tree - the tree to be printed
+ */
+void printBST(BST* tree);
+
+/**
+ * Prints a binary search tree in ascending order starting from a given node.
+ * @param node - the root node of the tree to be printed
+ * @param printFunction - a function which takes in a unit of data stored
+ *                        within the tree and prints it to the terminal
+ */
+void printSubtree(BSTNode* node, void (*printFunction)(void* data));
+
+/**
+ * Creates a new node for a binary search tree which contains the given data.
+ * @param data - the pointer to the data that will be stored by the new node
+ * @return a pointer to a new binary search tree node with null children and
+ *         a data pointer pointing to the given data
+ */
+BSTNode* createBSTNode(void* data);
+
+/**
+ * Creates a new node with given data and inserts the node to a binary search tree.
+ * @param tree - the tree which will contain the new data node
+ * @param data - the data which will be inserted to the tree
+ */
+void insertData(BST* tree, void* data);
+
+/**
+ * Deletes all occurences of a given data value in a binary search tree.
+ * @param tree - the tree to have its data deleted
+ * @param data - a pointer to data with the same values as what will be deleted
+ */
+void deleteData(BST* tree, void* data);
+
+/**
+ * Deletes all occurences of a given data value starting from a given root node
+ * in a binary search tree.
+ * @param tree - the tree to have its data deleted
+ * @param node - the root node of the subtree which will be searched
+ * @param data - a pointer to data with the same values as what will be deleted
+ * @return the new root node of the tree
+ */
+BSTNode* deleteDataRecursive(BST* tree, BSTNode* node, void* data);
+
+/**
+ * Finds the lowest value node below a given root node.
+ * @param node - the root of the subtree whose lowest value node will be found
+ * @return the node furthest down and to the left of the root
+ */
+BSTNode* findMin(BSTNode* node);
+
+/**
+ * Finds a node with a matching data value in a binary search tree.
+ * @param tree - the tree to be searched
+ * @param data - the data which is being searched for in the tree
+ * @return the first node found to contain the same value as data
+ */
+BSTNode* findData(BST* tree, void* data);
+
+#endif
